82881a6dff3e0c34bc45b22bae9576d0ace95495
Luke Hickey <lukehickey08@gmail.com>
2026-05-17 21:38:21 -0700

    Added greeting ui

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        paddingTop: 30,
+        paddingLeft: 30,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 35,
+          fontWeight: "700",
+          color: "#111",
+        }}
+      >
+        Hello Emerson,
+      </Text>
+    </View>
+  );
+}

653e8a1f27cb9fc2bcd7225cc912605b52d67303
Luke Hickey <lukehickey08@gmail.com>
2026-05-18 18:44:13 -0700

    created the main large button ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 24,
+      }}
+    >
       <View
         style={{
-          paddingTop: 30,
+          paddingTop: 50,
         }}
       >
         <Text
           style={{
             textAlign: "center",
-            fontSize: 35,
+            fontSize: 38,
             fontWeight: "700",
-            color: "#111",
+            color: "#222222",
           }}
         >
           Hello, Emerson
@@ -21,13 +26,53 @@ export default function Index() {
         <Text
           style={{
             textAlign: "center",
-            fontSize: 18,
+            fontSize: 20,
             fontWeight: "200",
           }}
         >
           {" "}
           Drive safe my love.
         </Text>
+      </View>
+
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          paddingTop: 45,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            width: 260,
+            height: 260,
+            borderRadius: 130,
+            borderColor: "#ffffff",
+            borderWidth: 7,
+            backgroundColor: "#F4B6C2",
+
+            justifyContent: "center",
+            alignItems: "center",
+
+            shadowColor: "#000",
+            shadowOpacity: 0.2,
+            shadowRadius: 15,
+            shadowOffset: {
+              width: 0,
+              height: 6,
+            },
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 32,
+              fontWeight: "700",
+              color: "#fff",
+            }}
+          >
+            START DRIVE
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );

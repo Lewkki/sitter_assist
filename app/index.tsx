b644f0caaeca943a847f3bfa5395294f2d03e8bf
Luke Hickey <lukehickey08@gmail.com>
2026-05-19 01:33:54 -0700

    added stat cards UI

--- app/index.tsx
+++ app/index.tsx
@@ -74,6 +74,92 @@ export default function Index() {
           </Text>
         </TouchableOpacity>
       </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          paddingBottom: 140,
+        }}
+      >
+        <View
+          style={{
+            width: "48%",
+            backgroundColor: "#A8D5BA",
+            padding: 20,
+
+            borderRadius: 24,
+            borderWidth: 7,
+            borderColor: "#b6e5c9",
+
+            shadowColor: "#000",
+            shadowOpacity: 0.2,
+            shadowRadius: 15,
+            shadowOffset: {
+              width: 0,
+              height: 6,
+            },
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 16,
+              color: "#fff",
+              fontWeight: "500",
+            }}
+          >
+            This Month
+          </Text>
+          <Text
+            style={{
+              marginTop: 10,
+              color: "#fff",
+              fontSize: 30,
+            }}
+          >
+            0 mi
+          </Text>
+        </View>
+
+        <View
+          style={{
+            width: "48%",
+            backgroundColor: "#F4B6C2",
+            padding: 20,
+
+            borderRadius: 24,
+            borderWidth: 7,
+            borderColor: "#fedbe3",
+
+            shadowColor: "#000",
+            shadowOpacity: 0.2,
+            shadowRadius: 15,
+            shadowOffset: {
+              width: 0,
+              height: 6,
+            },
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 16,
+              color: "#fff",
+              fontWeight: "500",
+            }}
+          >
+            This Year
+          </Text>
+          <Text
+            style={{
+              marginTop: 10,
+              color: "#fff",
+              fontSize: 30,
+            }}
+          >
+            0 mi
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
